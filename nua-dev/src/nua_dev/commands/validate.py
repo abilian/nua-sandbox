eb4ba0876bd3049d5d466971dd5d084ec35ff458
Stefane Fermigier <sf@fermigier.com>
2023-05-04 18:40:29 +0200

    feat: config validation.

---
+++ nua-dev/src/nua_dev/commands/validate.py
@@ -0,0 +1,41 @@
+"""Build a single app image."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from cleez import Argument, Command
+from cleez.colors import red, blue
+
+from nua_dev.config import Config, ConfigParseError
+from nua_dev.utils.backports import chdir
+
+
+class ValidateCommand(Command):
+    """Validate one or more Nua configs."""
+
+    name = "validate"
+
+    arguments = [
+        Argument(
+            "targets", nargs="*", help="Directories where to find the apps to validate"
+        )
+    ]
+
+    def run(self, targets: list[Path]):
+        if not targets:
+            targets = ["."]
+
+        for target in targets:
+            print(blue(f"Validating Nua config in: {target}"))
+            with chdir(target):
+                self._validate()
+
+    def _validate(self):
+        try:
+            config = Config.from_path(".")
+        except ConfigParseError as e:
+            print(red(e))
+            raise SystemExit(1)
+
+        config.validate()
