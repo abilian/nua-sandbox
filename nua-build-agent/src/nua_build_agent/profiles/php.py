80ef2973c5dc5dd8ab898cf76baf28e085c5148f
Stefane Fermigier <sf@fermigier.com>
2023-05-04 19:01:40 +0200

    fix: php builder (hack)

--- nua-build-agent/src/nua_build_agent/profiles/php.py
+++ nua-build-agent/src/nua_build_agent/profiles/php.py
@@ -8,11 +8,21 @@ class PhpProfile(BaseProfile):
     name = "php"
     label = "PHP / Composer"
     builder_packages = [
+        # Probably not needed at runtime
         "composer",
+        # Useful for composer install
+        "php-curl",
+        # Maybe not useful at runtime (or app-specific)
+        "php-xml",
     ]
 
     def accept(self):
         return self._check_files(["composer.json"])
 
     def build(self):
-        sh.shell("composer install")
+        try:
+            sh.shell("composer install")
+        except:
+            # FIXME: if this happens, the upstream package is probably broken.
+            sh.shell("composer update")
+            sh.shell("composer install")
