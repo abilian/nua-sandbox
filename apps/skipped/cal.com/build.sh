72c55ba49223c48ef89dd7d1ef8a21bf56e74cb5
Stefane Fermigier <sf@fermigier.com>
2023-05-16 15:13:08 +0200

    apps: stash cal.com for now.

--- apps/skipped/cal.com/build.sh
+++ apps/skipped/cal.com/build.sh
@@ -4,6 +4,9 @@ set -eux -o pipefail
 
 # See: https://github.com/calcom/docker/blob/main/Dockerfile
 
+#
+# NB: not used, see nua-config.toml for more info
+#
 yarn install
 yarn global add turbo
 yarn config set network-timeout 1000000000 -g
